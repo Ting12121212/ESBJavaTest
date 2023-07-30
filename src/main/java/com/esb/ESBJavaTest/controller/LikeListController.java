eabbd64896aad6eb486ee0e2be660248f5230b22
LAPTOP-MVR82OFG\Ting <0000@gmail.com>
2023-07-30 22:08:50 +0800

    更新功能(待完成)

--- src/main/java/com/esb/ESBJavaTest/controller/LikeListController.java
+++ src/main/java/com/esb/ESBJavaTest/controller/LikeListController.java
@@ -1,15 +1,18 @@
 package com.esb.ESBJavaTest.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.esb.ESBJavaTest.model.entity.LikeList;
 import com.esb.ESBJavaTest.model.entity.Products;
@@ -33,7 +36,6 @@ public class LikeListController {
 	private UsersService userService;
 
 	// ==========================================================
-
 	// 依UserID顯示對應的喜好紀錄
 	@GetMapping("/likeList/user/{userID}")
 	public String showLikeListByUserID(@PathVariable Integer userID, Model model) {
@@ -68,15 +70,48 @@ public class LikeListController {
 		likeList.setProducts(product);
 		likeListService.addToLikeList(likeList);
 	}
+
 	// =======================================================
 	// 依商品ID 刪除喜好紀錄表單一商品
+	@GetMapping("/likeList/deleteLikeList")
+	public String deleteShopCartProduct(@RequestParam Integer userID, @RequestParam Integer productNo) {
 
-		@GetMapping("/likeList/deleteLikeList")
-		public String deleteShopCartProduct(@RequestParam Integer userID, @RequestParam Integer productNo) {
+		likeListService.deleteLikeListByID(userID, productNo);
 
-			likeListService.deleteLikeListByID(userID, productNo);
+		return "updateLikeList";
+	}
 
-			return "redirect:/likeList/user/3";
-		}
+	// =======================================================
+	// 依喜好紀錄表ID 找出對應的商品資訊
+	@GetMapping("/likeList/sn")
+	public String deleteShopCartProduct(@RequestParam Integer sn, Model model) {
+
+		LikeList likeList = likeListService.findLikeListByID(sn);
+		model.addAttribute("likeList", likeList);
+
+		return "redirect:/likeList/user/3";
+	}
+
+	// =======================================================
+	// ===更新喜好紀錄表資訊
+//	@PostMapping("/shop/product/updateProduct/{productID}")
+//	public String updateProduct(@ModelAttribute("shopProduct") ShopProduct newProduct,
+//			@RequestParam("shopProduct.photo") MultipartFile photo) {
+//
+//		System.out.println("====================================" + newProduct);
+//
+//		try {
+//			if (!photo.isEmpty()) {
+//				byte[] photoData = photo.getBytes();
+//				newProduct.setPhoto(photoData);
+//			}
+//			productService.updateShopProductByID(newProduct.getProductID(), newProduct);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//		return "redirect:/shop/product/employeePage";
+//	}
 
 }
--- src/main/java/com/esb/ESBJavaTest/service/LikeListService.java
+++ src/main/java/com/esb/ESBJavaTest/service/LikeListService.java
@@ -1,6 +1,7 @@
 package com.esb.ESBJavaTest.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.esb.ESBJavaTest.model.DB_repository.LikeListRepository;
 import com.esb.ESBJavaTest.model.entity.LikeList;
+import com.esb.ESBJavaTest.model.entity.Products;
 
 @Service
 public class LikeListService {
@@ -28,7 +30,21 @@ public class LikeListService {
 		List<LikeList> likeLists = likeListDao.findLikeListsByUserID(userID);
 		return likeLists;
 	}
+	
+	// 依喜好紀錄ID取得對應資訊
+	public LikeList findLikeListByID(Integer sn) {
+		Optional<LikeList> option = likeListDao.findById(sn);
+		
+		if (option.isPresent()) {
+			return option.get();
+		} else {
+			LikeList error = new LikeList();
+			error.setAccount("沒有這筆資料");
+			return error;
+		}
+	}
 
+	
 	// =INSERT=============================================
 	
 	// 新增商品至喜好紀錄表
@@ -36,7 +52,9 @@ public class LikeListService {
 		likeListDao.save(likeList);
 	}
 
+	
 	// =DELETE=============================================
+	
 	// 依客人&商品ID 刪除喜好紀錄表單一商品
 		@Transactional
 		public Integer deleteLikeListByID(Integer userID, Integer productNo) {
